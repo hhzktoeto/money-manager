1aa91a3c287c491a1ed7dc4be2cf4e7f6780455f
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-10-02 16:42:50 +0300

    FEAT-unstable: added mobile navigation panel and navigation panel switching between desktop/mobile devices

--- app/src/main/java/hhz/ktoeto/moneymanager/ui/MainLayout.java
+++ app/src/main/java/hhz/ktoeto/moneymanager/ui/MainLayout.java
@@ -1,135 +1,62 @@
 package hhz.ktoeto.moneymanager.ui;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasElement;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.Scroller;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import hhz.ktoeto.moneymanager.ui.view.DashboardView;
 import hhz.ktoeto.moneymanager.ui.view.PlanningView;
 import hhz.ktoeto.moneymanager.ui.view.StatsView;
+import hhz.ktoeto.moneymanager.utils.RouterUtils;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Slf4j
 @UIScope
 @SpringComponent
-public class MainLayout extends Composite<Div>
-        implements RouterLayout, AfterNavigationObserver {
+public class MainLayout extends Composite<VerticalLayout> implements RouterLayout {
+    private final HorizontalLayout desktopNavigationContainer = new HorizontalLayout(
+            RouterUtils.createLink(DashboardView.class, "Главная", VaadinIcon.HOME),
+            RouterUtils.createLink(StatsView.class, "Статистика", VaadinIcon.PIE_BAR_CHART),
+            RouterUtils.createLink(PlanningView.class, "Планирование", VaadinIcon.CALC_BOOK)
+    );
+    private final HorizontalLayout mobileNavigationContainer = new HorizontalLayout(
+            RouterUtils.createLink(DashboardView.class, VaadinIcon.HOME),
+            RouterUtils.createLink(StatsView.class, VaadinIcon.PIE_BAR_CHART),
+            RouterUtils.createLink(PlanningView.class, VaadinIcon.CALC_BOOK)
+    );
 
-    private final Div outlet = new Div();                        // сюда рендерятся вьюхи
-    private final Tabs tabs = new Tabs();                        // вкладки навигации
-    private final Map<Tab, Class<? extends Component>> routeByTab = new LinkedHashMap<>();
-
-    // наш выезжающий блок и фон
-    private final Div menuPanel = new Div();
-    private final Div backdrop = new Div();
-    private boolean menuOpen = false;
+    private final HorizontalLayout headerContainer = new HorizontalLayout(new H1("Money Manager"), desktopNavigationContainer);
+    private final Div contentContainer = new Div();
 
     public MainLayout() {
-        // ---------- ШАПКА ----------
-        Button toggle = new Button(VaadinIcon.MENU.create(), e -> toggleMenu());
-        toggle.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_ICON);
+        VerticalLayout root = this.getContent();
+        root.addClassName("app-root");
+        root.setPadding(false);
+        root.setSpacing(false);
 
-        H1 title = new H1("Money Manager");
+        headerContainer.addClassName("app-header");
+        desktopNavigationContainer.addClassName("app-desktop-nav");
+        contentContainer.addClassName("app-content");
+        root.setFlexGrow(1, contentContainer);
+        mobileNavigationContainer.addClassName("app-mobile-nav");
 
-        Div navbar = new Div(toggle, title);
-        navbar.addClassNames("container", "app-navbar");
-        navbar.getStyle().set("height", "var(--app-navbar-height)");
-
-        // ---------- КОНТЕНТ ----------
-        outlet.setWidthFull();
-        Div container = new Div(outlet);
-        container.addClassName("container");
-
-        Div root = getContent();
-        root.setSizeFull();
-        root.add(navbar, container);
-
-        // ---------- МЕНЮ (собственный «sheet» без Dialog) ----------
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        tabs.addClassName("top-menu-tabs");
-        tabs.setWidthFull();
-
-        addTab("Главная", VaadinIcon.HOME,       DashboardView.class);
-        addTab("Статистика", VaadinIcon.PIE_BAR_CHART, StatsView.class);
-        addTab("Планирование", VaadinIcon.CALC_BOOK,   PlanningView.class);
-
-        tabs.addSelectedChangeListener(e -> {
-            Tab sel = e.getSelectedTab();
-            if (sel != null) {
-                Class<? extends Component> target = routeByTab.get(sel);
-                if (target != null) {
-                    UI.getCurrent().navigate(target);
-                    closeMenu();
-                }
-            }
-        });
-
-        // панель
-        menuPanel.addClassName("top-menu");
-        menuPanel.add(new Scroller(tabs));
-
-        // фон
-        backdrop.addClassName("top-menu-backdrop");
-        backdrop.addClickListener(e -> closeMenu()); // клик по фону закрывает меню
-
-        // кладём поверх всего (z-index берём из css)
-        root.add(menuPanel, backdrop);
+        root.add(headerContainer, contentContainer, mobileNavigationContainer);
     }
 
-    private void addTab(String text, VaadinIcon icon, Class<? extends Component> view) {
-        Tab t = new Tab(icon.create(), new Span(text));
-        routeByTab.put(t, view);
-        tabs.add(t);
-    }
-
-    private void openMenu() {
-        menuOpen = true;
-        menuPanel.getClassNames().add("open");
-        backdrop.getClassNames().add("open");
-    }
-
-    private void closeMenu() {
-        menuOpen = false;
-        menuPanel.getClassNames().remove("open");
-        backdrop.getClassNames().remove("open");
-    }
-
-    private void toggleMenu() {
-        if (menuOpen) closeMenu(); else openMenu();
-    }
-
-    // Куда класть дочерние вьюхи
     @Override
     public void showRouterLayoutContent(HasElement content) {
-        outlet.getElement().removeAllChildren();
-        if (content != null) outlet.getElement().appendChild(content.getElement());
-    }
-
-    // Подсветка активной вкладки и автозакрытие меню
-    @Override
-    public void afterNavigation(AfterNavigationEvent event) {
-        Class<?> target = event.getLocation().getClass();
-        routeByTab.entrySet().stream()
-                .filter(en -> en.getValue().equals(target))
-                .findFirst()
-                .ifPresent(en -> tabs.setSelectedTab(en.getKey()));
-        if (menuOpen) closeMenu();
+        if (content != null) {
+            contentContainer.getElement().removeAllChildren();
+            contentContainer.getElement().appendChild(content.getElement());
+        }
     }
 }
--- app/src/main/java/hhz/ktoeto/moneymanager/utils/RouterUtils.java
+++ app/src/main/java/hhz/ktoeto/moneymanager/utils/RouterUtils.java
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.theme.lumo.LumoUtility;
-import jakarta.validation.constraints.NotNull;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 public final class RouterUtils {
@@ -12,19 +12,27 @@ public final class RouterUtils {
     private RouterUtils() {
     }
 
-    public static RouterLink createLink(String viewName, Class<? extends Component> viewClass) {
+    public static RouterLink createLink(@NonNull Class<? extends Component> viewClass, @NonNull VaadinIcon icon) {
+        return doCreateLink(null, viewClass, icon);
+    }
+
+    public static RouterLink createLink(@NonNull Class<? extends Component> viewClass, @NonNull String viewName) {
         return doCreateLink(viewName, viewClass, null);
     }
 
-    public static RouterLink createLink(String viewName, Class<? extends Component> viewClass, @NotNull VaadinIcon icon) {
+    public static RouterLink createLink(@NonNull Class<? extends Component> viewClass, @NonNull String viewName, @NonNull VaadinIcon icon) {
         return doCreateLink(viewName, viewClass, icon);
     }
 
-    private static RouterLink doCreateLink(String viewName, Class<? extends Component> viewClass, @Nullable VaadinIcon icon) {
-        RouterLink link = new RouterLink(viewName, viewClass);
+    private static RouterLink doCreateLink(@Nullable String viewName, @NonNull Class<? extends Component> viewClass, @Nullable VaadinIcon icon) {
+        RouterLink link = new RouterLink(viewClass);
         if (icon != null) {
             link.add(icon.create());
         }
+        if (viewName != null) {
+            link.add(viewName);
+        }
+
         link.addClassNames(
                 LumoUtility.Display.FLEX,
                 LumoUtility.AlignItems.CENTER,
