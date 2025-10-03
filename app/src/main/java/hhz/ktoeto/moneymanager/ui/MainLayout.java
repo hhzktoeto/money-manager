11966d351f4074ebadd7edc666c3ada25feaad2d
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-10-03 12:39:43 +0300

    REFACTOR-unstable: moved main layout styles to separate css file

--- app/src/main/java/hhz/ktoeto/moneymanager/ui/MainLayout.java
+++ app/src/main/java/hhz/ktoeto/moneymanager/ui/MainLayout.java
@@ -1,19 +1,13 @@
 package hhz.ktoeto.moneymanager.ui;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasElement;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.spring.annotation.SpringComponent;
@@ -52,6 +46,7 @@ public class MainLayout extends Composite<VerticalLayout> implements RouterLayou
         );
         desktopRouters.forEach(router -> {
             router.addClassName("app-desktop-nav-buttons");
+            router.setHighlightCondition(HighlightConditions.sameLocation());
             desktopNavigationContainer.add(router);
         });
 
@@ -62,6 +57,7 @@ public class MainLayout extends Composite<VerticalLayout> implements RouterLayou
         );
         mobileRouters.forEach(router -> {
             router.addClassName("app-mobile-nav-buttons");
+            router.setHighlightCondition(HighlightConditions.sameLocation());
             mobileNavigationContainer.add(router);
         });
 
