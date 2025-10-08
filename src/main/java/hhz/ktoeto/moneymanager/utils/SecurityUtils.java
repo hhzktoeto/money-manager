d3b646416ff0d80c48b975c33e3f05dcb4a24b24
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-10-08 16:13:55 +0300

    CHORE-unstable: removed redundant annotations on util class

--- src/main/java/hhz/ktoeto/moneymanager/utils/SecurityUtils.java
+++ src/main/java/hhz/ktoeto/moneymanager/utils/SecurityUtils.java
@@ -1,16 +1,12 @@
 package hhz.ktoeto.moneymanager.utils;
 
 import hhz.ktoeto.moneymanager.backend.security.AppUserDetails;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
-@RequiredArgsConstructor
 public final class SecurityUtils {
 
     public static Long getCurrentUserId() {
