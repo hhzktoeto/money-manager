ba9b5f1f48e24925146eb507caaf840bf8482f9c
Dmitriy Goncharov <incoom@yandex.ru>
2025-10-19 17:55:04 +0300

    CHORE-unstable: updated category form styling

--- src/main/java/hhz/ktoeto/moneymanager/ui/feature/category/ui/form/CategoryForm.java
+++ src/main/java/hhz/ktoeto/moneymanager/ui/feature/category/ui/form/CategoryForm.java
@@ -2,10 +2,12 @@ package hhz.ktoeto.moneymanager.ui.feature.category.ui.form;
 
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import hhz.ktoeto.moneymanager.ui.feature.category.domain.Category;
 import hhz.ktoeto.moneymanager.ui.feature.category.ui.form.validator.CategoryNameValidator;
 import lombok.Getter;
@@ -30,17 +32,31 @@ public class CategoryForm extends Composite<FlexLayout> {
     protected FlexLayout initContent() {
         FlexLayout root = new FlexLayout();
         root.setFlexDirection(FlexLayout.FlexDirection.COLUMN);
+        root.addClassNames(
+                LumoUtility.MaxWidth.SCREEN_SMALL,
+                LumoUtility.Padding.NONE,
+                LumoUtility.Width.FULL,
+                LumoUtility.AlignItems.STRETCH
+        );
 
         nameField = new TextField("Имя");
+        nameField.setWidthFull();
         root.add(nameField);
 
         submitButton = new Button("Сохранить");
         submitButton.addClickListener(e -> submitAction.accept(this));
+        submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         cancelButton = new Button("Отмена");
         cancelButton.addClickListener(e -> cancelAction.accept(this));
 
         HorizontalLayout buttons = new HorizontalLayout(cancelButton, submitButton);
+        buttons.addClassNames(
+                LumoUtility.AlignItems.STRETCH,
+                LumoUtility.Margin.Top.MEDIUM,
+                LumoUtility.JustifyContent.BETWEEN,
+                LumoUtility.Gap.LARGE
+        );
         root.add(buttons);
 
         binder.forField(nameField)
