c6cd01cc5ab175b4664055b003b4dd221eb38f7f
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-10-28 15:27:22 +0300

    CHORE-unstable: added test case for budget renewer class test

--- src/test/java/feature/budget/domain/BudgetScheduledRenewerTest.java
+++ src/test/java/feature/budget/domain/BudgetScheduledRenewerTest.java
@@ -1,17 +1,19 @@
 package feature.budget.domain;
 
 import hhz.ktoeto.moneymanager.Launcher;
+import hhz.ktoeto.moneymanager.core.exception.EntityNotFoundException;
 import hhz.ktoeto.moneymanager.feature.budget.domain.Budget;
 import hhz.ktoeto.moneymanager.feature.budget.domain.BudgetRepository;
 import hhz.ktoeto.moneymanager.feature.budget.domain.BudgetScheduledRenewer;
-import hhz.ktoeto.moneymanager.feature.budget.domain.BudgetSpecification;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -26,21 +28,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(classes = Launcher.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class BudgetScheduledRenewerTest {
 
-    @Autowired
-    private BudgetRepository repository;
-    @Autowired
-    private BudgetScheduledRenewer renewer;
-
-    @PersistenceContext
-    private EntityManager em;
-
     private final String testBudgetName = "Test budget";
     private final long testUserId = 1L;
     private final LocalDate testBudgetStartDate = LocalDate.now().minusMonths(1);
     private final LocalDate testBudgetExpiredEndDate = LocalDate.now().minusDays(1);
     private final LocalDate testBudgetActiveEndDate = LocalDate.now().plusDays(1);
 
+    @Autowired
+    private BudgetRepository repository;
+    @Autowired
+    private BudgetScheduledRenewer renewer;
+    @PersistenceContext
+    private EntityManager em;
+
     @Test
+    @DirtiesContext
     void autoRenew_marksOldBudgetAsNotFavourite_andCreatesNew() {
         Budget expiredBudget = new Budget();
         expiredBudget.setName(testBudgetName);
@@ -54,7 +56,7 @@ class BudgetScheduledRenewerTest {
         repository.saveAndFlush(expiredBudget);
         em.clear();
 
-        renewer.autoRenew();
+        assertDoesNotThrow(() -> renewer.autoRenew());
 
         em.clear();
         Budget reloaded = repository.findById(expiredBudget.getId()).orElseThrow();
@@ -64,11 +66,7 @@ class BudgetScheduledRenewerTest {
                 () -> assertFalse(reloaded.isFavourite()),
                 () -> assertTrue(newBudget.isFavourite()),
                 () -> assertTrue(newBudget.isRenewable()),
-                () -> assertThat(repository.findAll(
-                        BudgetSpecification.builder()
-                                .userId(1L)
-                                .build()
-                )).hasSize(2),
+                () -> assertThat(repository.findAll()).hasSize(2),
                 () -> assertEquals(reloaded.getName(), newBudget.getName()),
                 () -> assertEquals(reloaded.isRenewable(), newBudget.isRenewable()),
                 () -> assertNotEquals(reloaded.isFavourite(), newBudget.isFavourite()),
@@ -77,6 +75,34 @@ class BudgetScheduledRenewerTest {
                 () -> assertEquals(reloaded.getActivePeriod(), newBudget.getActivePeriod()),
                 () -> assertEquals(reloaded.getGoalAmount(), newBudget.getGoalAmount()),
                 () -> assertEquals(reloaded.getUserId(), newBudget.getUserId())
+        );
+    }
+
+    @Test
+    @DirtiesContext
+    void autoRenew_noExpiredBudgetsPresent_shouldNotCreateOrUpdate() {
+        Budget activeBudget = new Budget();
+        activeBudget.setName(testBudgetName);
+        activeBudget.setUserId(testUserId);
+        activeBudget.setRenewable(true);
+        activeBudget.setFavourite(true);
+        activeBudget.setGoalAmount(BigDecimal.TEN);
+        activeBudget.setStartDate(testBudgetStartDate);
+        activeBudget.setEndDate(testBudgetActiveEndDate);
+
+        repository.saveAndFlush(activeBudget);
+        em.clear();
+
+        assertDoesNotThrow(() -> renewer.autoRenew());
+
+        em.clear();
+        Budget fetchedBudget = repository.findById(activeBudget.getId()).orElseThrow();
+
+        assertAll(
+                () -> assertThat(repository.findAll()).hasSize(1),
+                () -> assertThrows(EntityNotFoundException.class, () -> repository.findById(2L).orElseThrow(() -> new EntityNotFoundException(""))),
+                () -> assertThat(activeBudget).isEqualTo(fetchedBudget),
+                () -> assertTrue(fetchedBudget.isFavourite())
         );
     }
 }
