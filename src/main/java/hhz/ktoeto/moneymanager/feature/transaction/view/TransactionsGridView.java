705abfda62c7cc3c74585d04987ee3978c0094b8
Dmitriy Goncharov <incoom@yandex.ru>
2025-10-30 22:36:02 +0300

    CHORE-unstable: moved TransactionCategoryDateRenderer as nested class into TransactionsGridView, since its can only be used there

--- src/main/java/hhz/ktoeto/moneymanager/feature/transaction/view/TransactionsGridView.java
+++ src/main/java/hhz/ktoeto/moneymanager/feature/transaction/view/TransactionsGridView.java
@@ -5,15 +5,20 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.renderer.NumberRenderer;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import hhz.ktoeto.moneymanager.feature.category.domain.Category;
 import hhz.ktoeto.moneymanager.feature.transaction.domain.Transaction;
-import hhz.ktoeto.moneymanager.feature.transaction.view.renderer.TransactionCategoryDateRenderer;
 import hhz.ktoeto.moneymanager.ui.View;
 import hhz.ktoeto.moneymanager.ui.component.EmptyDataImage;
 
 import java.text.NumberFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Locale;
 
 public abstract class TransactionsGridView extends Composite<VerticalLayout> implements View {
@@ -80,11 +85,47 @@ public abstract class TransactionsGridView extends Composite<VerticalLayout> imp
                 .setSortable(this.isSortable())
                 .setPartNameGenerator(transaction -> {
                     StringBuilder stringBuilder = new StringBuilder("amount-column ");
-                    switch (transaction.getType()) {
-                        case EXPENSE -> stringBuilder.append("expense");
-                        case INCOME -> stringBuilder.append("income");
+                    if (transaction.getType() == Transaction.Type.EXPENSE) {
+                        stringBuilder.append("expense");
+                    } else {
+                        stringBuilder.append("income");
                     }
                     return stringBuilder.toString();
                 });
+    }
+
+    private static final class TransactionCategoryDateRenderer extends ComponentRenderer<HorizontalLayout, Transaction> {
+
+        //TODO: Добавить иконки для категория, когда они появятся
+        public TransactionCategoryDateRenderer() {
+            super(transaction -> {
+                HorizontalLayout layout = new HorizontalLayout();
+                layout.addClassNames(LumoUtility.Padding.XSMALL);
+                Category transactionCategory = transaction.getCategory();
+                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                        .appendPattern("dd MMMM yyyy")
+                        .toFormatter()
+                        .withLocale(Locale.of("ru"));
+
+                Span categoryName = new Span(transactionCategory.getName());
+                categoryName.addClassNames(
+                        LumoUtility.FontWeight.BOLD,
+                        LumoUtility.FontSize.MEDIUM
+                );
+                Span transactionDate = new Span(formatter.format(transaction.getDate()));
+                transactionDate.addClassNames(
+                        LumoUtility.TextColor.SECONDARY,
+                        LumoUtility.FontWeight.LIGHT,
+                        LumoUtility.FontSize.SMALL
+                );
+
+                VerticalLayout nameDateLayout = new VerticalLayout(categoryName, transactionDate);
+                nameDateLayout.setPadding(false);
+                nameDateLayout.addClassName(LumoUtility.Gap.XSMALL);
+
+                layout.add(nameDateLayout);
+                return layout;
+            });
+        }
     }
 }
