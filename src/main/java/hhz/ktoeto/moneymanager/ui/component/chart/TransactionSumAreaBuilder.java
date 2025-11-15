bc9755554c33cda11a1f9fe1e438acbae4e57bb2
Dmitriy Goncharov <incoom@yandex.ru>
2025-11-15 17:17:18 +0300

    FEAT-unstable(!): working on transactions monthly summaries charts styling

--- src/main/java/hhz/ktoeto/moneymanager/ui/component/chart/TransactionSumAreaBuilder.java
+++ src/main/java/hhz/ktoeto/moneymanager/ui/component/chart/TransactionSumAreaBuilder.java
@@ -1,20 +1,23 @@
 package hhz.ktoeto.moneymanager.ui.component.chart;
 
-import com.github.appreciated.apexcharts.config.builder.XAxisBuilder;
+import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.builder.*;
+import com.github.appreciated.apexcharts.config.chart.Type;
+import com.github.appreciated.apexcharts.config.responsive.builder.OptionsBuilder;
 import com.github.appreciated.apexcharts.config.xaxis.builder.LabelsBuilder;
 import com.github.appreciated.apexcharts.config.xaxis.labels.builder.StyleBuilder;
 import com.github.appreciated.apexcharts.helper.Series;
 import hhz.ktoeto.moneymanager.feature.statistics.domain.dto.TransactionSum;
+import hhz.ktoeto.moneymanager.ui.constant.StyleConstants;
 
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
 
-public class TransactionSumAreaBuilder extends AreaChartBuilder {
+public class TransactionSumAreaBuilder extends ApexChartsBuilder {
 
     public TransactionSumAreaBuilder(List<TransactionSum> data) {
-        super();
         String[] xLabels = data.stream()
                 .map(transactionSum -> {
                     YearMonth yearMonth = YearMonth.of(transactionSum.year(), transactionSum.month());
@@ -28,11 +31,56 @@ public class TransactionSumAreaBuilder extends AreaChartBuilder {
                 .map(transactionSum -> transactionSum.incomesSum().doubleValue())
                 .toArray(Double[]::new);
 
-        this.withXaxis(XAxisBuilder.get()
+        this.withChart(ChartBuilder.get()
+                        .withType(Type.BAR)
+                        .withHeight("500")
+                        .build())
+                .withLegend(LegendBuilder.get()
+                        .withShow(false)
+                        .build())
+                .withDataLabels(DataLabelsBuilder.get()
+                        .withEnabled(true)
+                        .withStyle(com.github.appreciated.apexcharts.config.datalables.builder.StyleBuilder.get()
+                                .withFontSize("10px")
+                                .withFontFamily(StyleConstants.FontFamily.MAIN_FONT)
+                                .build())
+                        .build())
+                .withYaxis(YAxisBuilder.get()
+                        .withLabels(com.github.appreciated.apexcharts.config.yaxis.builder.LabelsBuilder.get()
+                                .withStyle(com.github.appreciated.apexcharts.config.yaxis.labels.builder.StyleBuilder.get()
+                                        .withFontSize("12px")
+                                        .build())
+                                .build())
+                        .build())
+                .withXaxis(XAxisBuilder.get()
                         .withCategories(xLabels)
                         .withLabels(LabelsBuilder.get()
+                                .withRotate(-45.0)  // Поворот для длинных лейблов
                                 .withStyle(StyleBuilder.get()
-                                        .withColors(List.of("#d8dee9"))
+                                        .withFontSize("12px")
+                                        .build())
+                                .build())
+                        .build())
+                .withTooltip(TooltipBuilder.get()
+                        .withEnabled(true)
+                        .withFillSeriesColor(false)
+                        .build())
+                .withResponsive(ResponsiveBuilder.get()
+                        .withBreakpoint(480.0)
+                        .withOptions(OptionsBuilder.get()
+                                .withChart(ChartBuilder.get()
+                                        .withHeight("300")
+                                        .build())
+                                .withDataLabels(DataLabelsBuilder.get()
+                                        .withEnabled(false)  // Скрыть на мобильке
+                                        .build())
+                                .withXAxis(XAxisBuilder.get()
+                                        .withLabels(LabelsBuilder.get()
+                                                .withRotate(-90.0)  // Более крутой поворот
+                                                .withStyle(StyleBuilder.get()
+                                                        .withFontSize("10px")
+                                                        .build())
+                                                .build())
                                         .build())
                                 .build())
                         .build())
@@ -40,3 +88,4 @@ public class TransactionSumAreaBuilder extends AreaChartBuilder {
                 .withColors("#bf616a", "#a3be8c");
     }
 }
+
